382e23807981d968b97d6583e583e161df8c9a71
Iliyan Kamilov <ilian.kamilov@gmail.com>
2013-01-03 22:12:43 +0200

    Fix LoggerFactory.java - log in app server log file.

--- biol-util/src/main/java/com/biol/biolbg/util/logger/LoggerFactory.java
+++ biol-util/src/main/java/com/biol/biolbg/util/logger/LoggerFactory.java
@@ -1,64 +1,20 @@
 package com.biol.biolbg.util.logger;
 
-import java.io.IOException;
-import java.util.logging.FileHandler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
-
-import javax.annotation.PostConstruct;
-import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
-import javax.inject.Inject;
+import javax.enterprise.inject.spi.InjectionPoint;
 
-import com.biol.biolbg.util.configuration.ApplicationConfiguration;
-
-@ApplicationScoped
 public class LoggerFactory
 {
-	private static final String PARENT_LOGGER_NAME = "com.biol";
-
-	private static final String LOGGER_NAME = "com.biol.biolbg";
-
-	@Inject
-	private ApplicationConfiguration applicationConfiguration;
-
-	@PostConstruct
-	public void postConstruct()
+	@Produces
+	public static Logger createLogger(InjectionPoint injectionPoint)
 	{
-		initParentLogger();
-	}
+		String name = injectionPoint.getMember().getDeclaringClass().getName();
+		Logger logger = Logger.getLogger(name);
 
-	@Produces Logger createLogger()
-	{
-		Logger logger = Logger.getLogger(LOGGER_NAME);
-		logger.setUseParentHandlers(true);
 		logger.setLevel(Level.ALL);
 
 		return logger;
-	}
-
-	private void initParentLogger()
-	{
-		Logger parentLogger = Logger.getLogger(PARENT_LOGGER_NAME);
-
-		String logPath = applicationConfiguration.getLogPath();
-
-		try
-		{
-			int limit = 1024 * 1024;
-			FileHandler handler = new FileHandler(logPath, limit, 1, true);
-			handler.setFormatter(new SimpleFormatter());
-			parentLogger.addHandler(handler);
-			parentLogger.setUseParentHandlers(false);
-		}
-		catch (SecurityException e)
-		{
-			e.printStackTrace();
-		}
-		catch (IOException e)
-		{
-			e.printStackTrace();
-		}
 	}
 }
